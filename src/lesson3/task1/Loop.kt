ae01a481a122be83b9973c94c3e292acaa2ec6c0
charlescoughlin <aassggw@gmail.com>
2017-10-24 19:09:15 +0300

    lesson 3 + fixed l1 and l2

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -60,7 +60,15 @@ fun digitCountInNumber(n: Int, m: Int): Int =
  * Найти количество цифр в заданном числе n.
  * Например, число 1 содержит 1 цифру, 456 -- 3 цифры, 65536 -- 5 цифр.
  */
-fun digitNumber(n: Int): Int = TODO()
+fun digitNumber(n: Int): Int {
+    var count = 0
+    var i = n
+    while (i != 0) {
+        count ++
+        i /= 10
+    }
+    return if (count == 0) 1 else count
+}
 
 /**
  * Простая
@@ -68,7 +76,18 @@ fun digitNumber(n: Int): Int = TODO()
  * Найти число Фибоначчи из ряда 1, 1, 2, 3, 5, 8, 13, 21, ... с номером n.
  * Ряд Фибоначчи определён следующим образом: fib(1) = 1, fib(2) = 1, fib(n+2) = fib(n) + fib(n+1)
  */
-fun fib(n: Int): Int = TODO()
+fun fib(n: Int): Int {
+    var n1 = 1
+    var n2 = 1
+    var Numb = 1
+    for (i in 3..n) {
+        Numb = n1 + n2
+        val s = n2
+        n2 += n1
+        n1 = s
+    }
+    return Numb
+}
 
 /**
  * Простая
@@ -76,21 +95,47 @@ fun fib(n: Int): Int = TODO()
  * Для заданных чисел m и n найти наименьшее общее кратное, то есть,
  * минимальное число k, которое делится и на m и на n без остатка
  */
-fun lcm(m: Int, n: Int): Int = TODO()
+fun lcm(m: Int , n: Int): Int {
+    val c = m * n
+    var a = m
+    var b = n
+    while ((a != 0) && (b != 0)) {
+        if (a > b) a %= b else b %= a
+    }
+    return c / (a + b)
+}
 
 /**
  * Простая
  *
  * Для заданного числа n > 1 найти минимальный делитель, превышающий 1
  */
-fun minDivisor(n: Int): Int = TODO()
+fun minDivisor(n: Int): Int {
+    var min = 0
+    for (i in 2..n) {
+        if ((n % i) == 0) {
+            min = i
+            break
+        }
+    }
+    return min
+}
 
 /**
  * Простая
  *
  * Для заданного числа n > 1 найти максимальный делитель, меньший n
  */
-fun maxDivisor(n: Int): Int = TODO()
+fun maxDivisor(n: Int): Int {
+    var max = 0
+    for (i in (n - 1)..1) {
+        if ((n % i) == 0) {
+            max = i
+            break
+        }
+    }
+    return max
+}
 
 /**
  * Простая
@@ -99,7 +144,13 @@ fun maxDivisor(n: Int): Int = TODO()
  * Взаимно простые числа не имеют общих делителей, кроме 1.
  * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
  */
-fun isCoPrime(m: Int, n: Int): Boolean = TODO()
+fun isCoPrime(m: Int, n: Int): Boolean {
+    val max = maxOf(m, n)
+    for (i in 2..max) {
+        if (((m % i) == 0) && ((n % i) == 0)) return false
+    }
+    return true
+}
 
 /**
  * Простая
